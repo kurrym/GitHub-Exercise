6dd86d6ee3606bd59aaaa2717c38f0873798c79c
kurrym <mckinson.kerry@gmail.com>
2023-10-19 16:38:40 -0400

    Initial Commit

---
+++ GitHub Exercise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHub Exercise
+//
+//  Created by Kerry Mckinson on 2023-10-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("V 1.0")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by Kerry Mckinson on 2023-10-19.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
